08822158b1c89d369d591dae87c8c9503a4d0305
Student Pratikshashinde28 <pratikshashinde7449@gmail.com>
2026-01-22 13:29:31 +0530

    add new folder

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+{
+    username:{
+        type: string,
+        required: true,
+        unique: true
+     },
+
+    email:{
+        type: string,
+        reuired: true,
+        unique: true
+     },
+
+    password :{
+        type: string,
+        required: [true,"Password is required"]
+    }
+},
+{timestamps: true}
+);
+
+module.export = mongoose.model("User",UserSchema);
